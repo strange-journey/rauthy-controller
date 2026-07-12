12ded9f6ad8d78416d5c5561a1dd4bb353c1923b
june <dev@bug.jp.net>
2026-07-11 17:27:22 -0700

    Patch out extraneous fields from crdgen

--- src/crdgen.rs
+++ src/crdgen.rs
@@ -3,6 +3,22 @@ use rauthy_controller::controller;
 
 fn main() {
     let mut crd = controller::OIDCClient::crd();
+    if crd.spec.names.categories.as_ref().is_some_and(Vec::is_empty) {
+        crd.spec.names.categories = None;
+    }
+    if crd.spec.names.short_names.as_ref().is_some_and(Vec::is_empty) {
+        crd.spec.names.short_names = None;
+    }
+    for version in &mut crd.spec.versions {
+        if version
+            .additional_printer_columns
+            .as_ref()
+            .is_some_and(Vec::is_empty)
+        {
+            version.additional_printer_columns = None;
+        }
+    }
+
     crd.metadata
         .annotations
         .get_or_insert_with(Default::default)
